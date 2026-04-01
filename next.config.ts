90f720e9019387be1354c0a53866dda41396b337
andresucv96 <andresucv96@gmail.com>
2026-04-01 10:29:34 +0200

    langchain dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: [
+    "langchain",
+    "@langchain/core",
+    "@langchain/anthropic",
+    "@langchain/langgraph",
+  ],
 };
 
 export default nextConfig;
